85b588279675d467c1416cafd1a2c6ebc0980f64
Marcos Lopez Medina <166430537+Marrkitu2@users.noreply.github.com>
2024-04-10 10:02:13 +0200

    Update p168.java

--- Exercises/p168.java
+++ Exercises/p168.java
@@ -14,7 +14,6 @@ public class AceptaElReto168 {
        }
     }
 
-    // ir a la posicion a[n]
     public static int contarNumeros(int n1) {
         int[] a = new int[n1 -1];
         int i = 0;
@@ -25,7 +24,7 @@ public class AceptaElReto168 {
             for (i = 0; i < n1 -1; i++){
                 a[i] = sc.nextInt();
             }
-            ordenarEnteros(a);//ordenar la array
+            ordenarEnteros(a);
             for (i=0; i < n1 -1; i++){
                 if (a[i] != i +1){
                     return(i+1);
@@ -37,9 +36,7 @@ public class AceptaElReto168 {
     public static void ordenarEnteros(int[] n){
         int t;
         for (int i = 0; i < n.length - 1; i++) {
-            // Agafa el segon valor entre el següent al primer i l'últim
             for (int j = i + 1; j < n.length; j++) {
-                // Intercanviar si el primer és més gran que el segon
                 if (n[i] > n[j]) {
                     t = n[i];
                     n[i] = n[j];
